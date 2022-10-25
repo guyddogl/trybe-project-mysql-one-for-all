7e19cf07f48652c4e85152ae84a71ecd4cc4af1a
Guyddo <guyddogl@gmail.com>
2022-10-25 18:01:53 -0300

    Refatora query

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,4 @@
-SELECT
-	COUNT(songs.name) AS 'cancoes',
-    COUNT(DISTINCT artists.name) AS 'artistas',
-    COUNT(DISTINCT albums.name) AS 'albuns'
+SELECT COUNT(songs.name) AS 'cancoes', COUNT(DISTINCT artists.name) AS 'artistas', COUNT(DISTINCT albums.name) AS 'albuns'
 FROM SpotifyClone.songs
 RIGHT JOIN SpotifyClone.albums
 ON albums.id = songs.album
