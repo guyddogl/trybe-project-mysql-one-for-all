165b1ca58bfaea7a92e3dab4b4bb717bfc6457c9
Guyddo <guyddogl@gmail.com>
2022-10-25 17:53:04 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT
+	COUNT(songs.name) AS 'cancoes',
+    COUNT(DISTINCT artists.name) AS 'artistas',
+    COUNT(DISTINCT albums.name) AS 'albuns'
+FROM SpotifyClone.songs
+RIGHT JOIN SpotifyClone.albums
+ON albums.id = songs.album
+RIGHT JOIN SpotifyClone.artists
+ON albums.artist = artists.id;
